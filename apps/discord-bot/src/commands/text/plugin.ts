36d3dee54d7ba7fdac2223d96343c93a8372ec5e
DuroCodes <davidwright13503@gmail.com>
2022-11-09 15:35:47 -0500

    feat: fix typos & move things around for test bot

---
+++ apps/discord-bot/src/commands/text/plugin.ts
@@ -0,0 +1,10 @@
+import { SparkCommand, CommandType } from '@spark.ts/handler';
+import { ownerOnlyText } from '../../plugins/ownerOnly';
+
+export default new SparkCommand({
+  type: CommandType.Text,
+  plugins: [ownerOnlyText(['283312847478325251'])],
+  run({ message }) {
+    message.reply('You are allowed to use this command!');
+  },
+});
---
+++ apps/discord-bot/src/plugins/ownerOnly.ts
@@ -0,0 +1,41 @@
+import { SlashCommandPlugin, TextCommandPlugin } from '@spark.ts/handler';
+
+function mapUsers(ids: string[]) {
+  const userMention = (s: string) => `<@!${s}>`;
+  return ids.map((id) => `\` - \` ${userMention(id)}`).join('\n');
+}
+
+export function ownerOnlySlash(ownerIds: string[]): SlashCommandPlugin {
+  return {
+    description: 'Only allows the owner to run the command.',
+    async run({ interaction, controller }) {
+      if (ownerIds.includes(interaction.user.id)) {
+        return controller.next();
+      }
+
+      await interaction.reply({
+        content: `Only these people can run this!\n${mapUsers(ownerIds)}`,
+        ephemeral: true,
+      });
+
+      return controller.stop();
+    },
+  };
+}
+
+export function ownerOnlyText(ownerIds: string[]): TextCommandPlugin {
+  return {
+    description: 'Only allows the owner to run the command.',
+    async run({ message, controller }) {
+      if (ownerIds.includes(message.author.id)) {
+        return controller.next();
+      }
+
+      await message.reply({
+        content: `Only these people can run this!\n${mapUsers(ownerIds)}`,
+      });
+
+      return controller.stop();
+    },
+  };
+}
