e9f0c85f4c0735785a97ee4bb0e7993d52adfea5
DuroCodes <davidwright13503@gmail.com>
2023-01-28 14:36:39 -0500

    feat(docs): add api docs for guards

--- apps/docs/src/utils/docgen.mjs
+++ apps/docs/src/utils/docgen.mjs
@@ -6,29 +6,51 @@ async function main() {
   const docFiles = (await glob('src/pages/docs/**/*.mdx')).filter((v) => !v.includes('index.mdx'));
 
   if (!docFiles.length) {
-    return createDocumentation({
+    await createDocumentation({
       input: [`${process.cwd()}/../../packages/handler/src/index.ts`],
-      output: `${process.cwd()}/src/pages/docs`,
+      output: `${process.cwd()}/src/pages/docs/handler`,
       tsconfigPath: `${process.cwd()}/../../packages/handler/tsconfig-base.json`,
       markdown: true,
     });
+
+    await createDocumentation({
+      input: [`${process.cwd()}/../../packages/guards/src/index.ts`],
+      output: `${process.cwd()}/src/pages/docs/guards`,
+      tsconfigPath: `${process.cwd()}/../../packages/guards/tsconfig.json`,
+      markdown: true,
+    });
+
+    return;
   }
 
   const paths = docFiles.map((v) => ({
     path: v,
     name: v.split('/').at(-1),
-    folder: v.split('/').at(-4),
+    folder: `${v.split('/').at(-4)}/${v.split('/').at(-2)}`,
   }));
 
+  const folders = [...new Set(paths.map(({ folder }) => folder.split('/').at(-1)))];
+  const subFolders = [...new Set(paths.map(({ folder }) => folder.split('/').at(0)))];
+
   paths.forEach(({ path, folder, name }) => {
-    fs.renameSync(path, `src/pages/docs/${folder}/${name}`);
+    fs.renameSync(path, `src/pages/docs/${folder.split('/').reverse().join('/')}/${name}`);
   });
 
-  const folders = [...new Set(paths.map(({ folder }) => folder))];
-
   folders.forEach((folder) => {
-    fs.rmdirSync(`src/pages/docs/${folder}/@spark.ts/handler`);
-    fs.rmdirSync(`src/pages/docs/${folder}/@spark.ts`);
+    const meta = folder !== 'guards'
+      ? { classes: 'Classes', types: 'Types' }
+      : { classes: 'Classes' };
+
+    fs.writeFileSync(`src/pages/docs/${folder}/_meta.json`, JSON.stringify(meta, null, 2));
+
+    subFolders.forEach((sub) => {
+      try {
+        fs.rmdirSync(`src/pages/docs/${folder}/${sub}/@spark.ts/${folder}`);
+        fs.rmdirSync(`src/pages/docs/${folder}/${sub}/@spark.ts`);
+      } catch (e) {
+        //
+      }
+    });
   });
 
   const newFiles = await glob('src/pages/docs/**/*.mdx');
@@ -51,20 +73,19 @@ async function main() {
       })
       .join('\n');
 
-    const newData = `\
----
+    const newData = `---
 title: ${file.split('/').at(-1).split('.')[0]}
 ---
 
 ${replacedData}`;
 
     fs.writeFileSync(file, newData);
+    fs.writeFileSync('src/pages/docs/_meta.json', JSON.stringify({
+      index: 'Api Docs',
+      handler: 'Handler',
+      guards: 'Guards',
+    }));
   });
-
-  fs.writeFileSync('src/pages/docs/_meta.json', JSON.stringify({
-    classes: 'Classes',
-    types: 'Types',
-  }, null, 2));
 
   fs.writeFileSync('src/pages/docs/index.mdx', `---
 title: API Docs
@@ -79,7 +100,7 @@ In this section of the docs, you can view info on the classes and types, and vie
 
 <Callout>
   This section is in beta, it may have some errors.
-  For now, it's best to use the \`Guide\` section to learn more.
+  For now, it's best to use the [guide](/guide) section to learn more.
 </Callout>
 `);
 }
